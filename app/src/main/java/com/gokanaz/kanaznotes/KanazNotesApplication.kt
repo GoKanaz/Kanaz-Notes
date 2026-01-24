2599ed574b40eae604055b27520b02c0e275f606
GoKanaz550 <gokanaz.official@gmail.com>
2026-01-24 14:43:55 +0700

    Fix SDK and disable MMKV init

--- app/src/main/java/com/gokanaz/kanaznotes/KanazNotesApplication.kt
+++ app/src/main/java/com/gokanaz/kanaznotes/KanazNotesApplication.kt
@@ -1,11 +1,7 @@
 package com.gokanaz.kanaznotes
-
 import android.app.Application
-import com.tencent.mmkv.MMKV
-
 class KanazNotesApplication : Application() {
     override fun onCreate() {
         super.onCreate()
-        MMKV.initialize(this)
     }
 }
