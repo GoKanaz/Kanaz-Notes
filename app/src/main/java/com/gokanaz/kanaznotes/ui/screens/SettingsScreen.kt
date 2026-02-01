26a49e8a08fb95c9b8dad63b6fef297249e3a595
GoKanaz <gokanaz.official@gmail.com>
2026-02-01 15:43:15 +0700

    fix: use Icons.Outlined.ArrowBack

--- app/src/main/java/com/gokanaz/kanaznotes/ui/screens/SettingsScreen.kt
+++ app/src/main/java/com/gokanaz/kanaznotes/ui/screens/SettingsScreen.kt
@@ -4,7 +4,6 @@ import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.autoMirrored.default.ArrowBack
 import androidx.compose.material.icons.outlined.*
 import androidx.compose.material3.*
 import androidx.compose.runtime.Composable
@@ -26,7 +25,7 @@ fun SettingsScreen(
                 title = { Text("Pengaturan") },
                 navigationIcon = {
                     IconButton(onClick = { navController.popBackStack() }) {
-                        Icon(Icons.AutoMirrored.Default.ArrowBack, null)
+                        Icon(Icons.Outlined.ArrowBack, null)
                     }
                 }
             )
